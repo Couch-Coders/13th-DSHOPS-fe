6c4bc70583dc28430ee9a3e742bb10ae3de821be
jayleeforever <jayforgood@gmail.com>
2023-03-10 17:50:28 +0900

    수정1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div>수정</div>
+      <div>수정1</div>
     </div>
   );
 }
